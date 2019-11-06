746310ef6d7c3a9301cc721e0495a0f501549911
ishijima <cgebtaro0221@gmail.com>
2019-11-07 01:15:55 +0900

    initial commit

---
+++ app/src/main/java/com/websarva/wings/android/appsample/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.websarva.wings.android.appsample
+
+import android.content.Intent
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+
+    var _busStopNamesStr = ""
+    var _busStopTimetableStr = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val receiver = OpenDataReceiver()
+        receiver.execute()
+        val btSearch = findViewById<Button>(R.id.btSearch)
+        btSearch.setOnClickListener(SearchListener())
+    }
+
+    private inner class SearchListener : View.OnClickListener {
+        override fun onClick(view: View) {
+            val etDeparture = findViewById<EditText>(R.id.etDeparture)
+            val etArrival = findViewById<EditText>(R.id.etArrival)
+            val departureStr = etDeparture.text.toString()
+            val arrivalStr = etArrival.text.toString()
+            val intent = Intent(applicationContext, SearchResultActivity::class.java)
+            intent.putExtra("departureStr", departureStr)
+            intent.putExtra("arrivalStr", arrivalStr)
+            intent.putExtra("busStopNamesStr", _busStopNamesStr)
+            intent.putExtra("busStopTimetableStr", _busStopTimetableStr)
+            startActivity(intent)
+        }
+    }
+
+    private inner class OpenDataReceiver() : AsyncTask<String, String, String>() {
+        override fun doInBackground(vararg params: String): String {
+            val TOKEN = "7c65e1e1f0ce05a6f5563fc18bd07676bd4996e704ed76c3a96f8c7f97f746f0"
+            val urlStr =
+                "https://api-tokyochallenge.odpt.org/api/v4/odpt:BusTimetable?dc:title=%E9%83%BD%EF%BC%90%EF%BC%92%20%E5%A4%A7%E5%A1%9A%E9%A7%85%E5%89%8D%E8%A1%8C&odpt:operator=odpt.Operator:Toei&acl:consumerKey=${TOKEN}"
+            val url = URL(urlStr)
+            val con = url.openConnection() as HttpURLConnection
+            con.requestMethod = "GET"
+            con.connect()
+            val stream = con.inputStream
+            val result = is2String(stream)
+            con.disconnect()
+            stream.close()
+            return result
+        }
+
+        override fun onPostExecute(result: String) {
+            val jArray = JSONArray(result)
+            val jObject = jArray.getJSONObject(0)
+            val busTimetable = jObject.getString("odpt:busTimetableObject")
+            val busTimetableObjects = JSONArray(busTimetable)
+            val busStopNames: MutableList<String> = mutableListOf()
+            val busStopTimetable: MutableList<String> = mutableListOf()
+            for(i in 0..(busTimetableObjects.length()-1)) {
+                val BusTimetableObject = busTimetableObjects.getJSONObject(i)
+                busStopNames.add(BusTimetableObject.getString("odpt:note").replace(":\\d+:\\d+".toRegex(),""))
+                busStopTimetable.add(BusTimetableObject.getString("odpt:arrivalTime"))
+            }
+            _busStopNamesStr = busStopNames.toString()
+            _busStopTimetableStr = busStopTimetable.toString()
+            val name = _busStopNamesStr
+            val bus = _busStopTimetableStr
+        }
+    }
+
+    private fun is2String(stream: InputStream): String {
+        val sb = StringBuilder()
+        val reader = BufferedReader(InputStreamReader(stream, "UTF-8"))
+        var line = reader.readLine()
+        while (line != null) {
+            sb.append(line)
+            line = reader.readLine()
+        }
+        reader.close()
+        return sb.toString()
+    }
+}
---
+++ app/src/main/java/com/websarva/wings/android/appsample/SearchResultActivity.kt
@@ -0,0 +1,213 @@
+package com.websarva.wings.android.appsample
+
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ListView
+import android.widget.SimpleAdapter
+import android.widget.TextView
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.text.SimpleDateFormat
+
+class SearchResultActivity : AppCompatActivity() {
+    private val _valueList: MutableList<MutableMap<String, Any>> = mutableListOf()
+    private var _counter = 0
+//    private var _range = 0..0
+    private val _walkingTimes = mutableListOf<String>()
+    private val _distanceTexts = mutableListOf<String>()
+    private val _from = mutableListOf<String>()
+    private val _to = mutableListOf<String>()
+    private val _heightDifs = mutableListOf<Float>()
+    private val _timeDifs = mutableListOf<Int>()
+    private var _busStopNames = mutableListOf<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_result)
+
+        // 前のアクティビティからフォームに入力された出発地・到着地のデータを受け取る
+        val departureStr = intent.getStringExtra("departureStr")
+        val arrivalStr = intent.getStringExtra("arrivalStr")
+        val busStopNamesStr = intent.getStringExtra("busStopNamesStr")
+        // 文字列データをJSONArrayに変換
+        val busStopNames = JSONArray(intent.getStringExtra("busStopNamesStr"))
+        // 時刻表のデータにはJSONArrayが使えなかったので一旦そのまま変数に代入
+        var busStopTimetable = intent.getStringExtra("busStopTimetableStr")
+        // busStopNamesのJSONArrayをListに変換するための入れ物を用意
+        val arrayList = mutableListOf<String>()
+        // JSONArrayをListに変換(indexOfを使えるようにするため)
+        for(i in 0..(busStopNames.length()-1)){
+            arrayList.add(busStopNames.get(i).toString())
+        }
+        // JSONArrayがbusStopTimetableに使えなかったので[]を除去してsplitでリスト化
+        busStopTimetable = busStopTimetable.replace("[", "")
+        busStopTimetable = busStopTimetable.replace("]", "")
+        val TimetbleArray = busStopTimetable.split(",")
+        // 前のアクティビティで入力された出発地・到着地のindex番号(バス停番号)を求める
+        val departureIndex = arrayList.indexOf(departureStr)
+        val arrivalIndex = arrayList.indexOf(arrivalStr)
+//        _range = departureIndex..arrivalIndex-1
+        _busStopNames = arrayList
+        // 出発地と到着地の間にある区間の数だけWebAPIに接続したいのでループを回す
+        for(i in departureIndex..(arrivalIndex-1)){
+            val receiver = MapDataReceiver()
+            receiver.execute(arrayList[i], arrayList[i+1])
+            // バス停間のバス移動時間も求める
+            _timeDifs.add(getTimeDif(TimetbleArray[i], TimetbleArray[i+1]))
+        }
+    }
+
+    // Google Maps APIに接続してそれぞれのバス停の緯度経度、区間の徒歩時間、区間距離を求める
+    private inner class MapDataReceiver() : AsyncTask<String, String, String>() {
+        override fun doInBackground(vararg params: String): String {
+            val TOKEN = "AIzaSyAovx0o3cTzyPimFYE2ROppc6LCqt75p1g"
+            val departureStr = params[0]
+            val arrivalStr = params[1]
+            val urlStr =
+                "https://maps.googleapis.com/maps/api/directions/json?origin=${departureStr}&destination=${arrivalStr}&mode=walking&key=${TOKEN}"
+            val url = URL(urlStr)
+            val con = url.openConnection() as HttpURLConnection
+            con.requestMethod = "GET"
+            con.connect()
+            val stream = con.inputStream
+            val result = is2String(stream)
+            con.disconnect()
+            stream.close()
+            return result
+        }
+
+        override fun onPostExecute(result: String) {
+            val jObject = JSONObject(result)
+            val routes = jObject.getString("routes")
+            val jArray = JSONArray(routes)
+            val bounds = jArray.getJSONObject(0)
+            val legs = bounds.getString("legs")
+            val jsonArray = JSONArray(legs)
+            val jsonObject = jsonArray.getJSONObject(0)
+            val distance = jsonObject.getString("distance")
+            val distanceObj = JSONObject(distance)
+            _distanceTexts.add(distanceObj.getString("text"))
+            val distanceTexts = _distanceTexts
+            val duration = jsonObject.getString("duration")
+            val durationObj = JSONObject(duration)
+            _walkingTimes.add(durationObj.getString("value"))
+            val walkingTimes = _walkingTimes
+            val endLocation = jsonObject.getString("end_location")
+            val startLocation = jsonObject.getString("start_location")
+            val endObj = JSONObject(endLocation)
+            val startObj = JSONObject(startLocation)
+            val endLat = endObj.getString("lat")
+            val endLng = endObj.getString("lng")
+            val startLat = startObj.getString("lat")
+            val startLng = startObj.getString("lng")
+            val StartElevationReceiver = StartElevationReceiver()
+            StartElevationReceiver.execute(startLng, startLat, endLng, endLat)
+        }
+    }
+
+    // 区間ごとの出発地となるバス停の標高を求める
+    private inner class StartElevationReceiver() : AsyncTask<String, String, String>() {
+        var _endLng = ""
+        var _endLat = ""
+        override fun doInBackground(vararg params: String): String {
+            val startLng = params[0]
+            val startLat = params[1]
+            _endLng = params[2]
+            _endLat = params[3]
+            val urlStr =
+                "http://cyberjapandata2.gsi.go.jp/general/dem/scripts/getelevation.php?lon=${startLng}&lat=${startLat}&outtype=JSON"
+            val url = URL(urlStr)
+            val con = url.openConnection() as HttpURLConnection
+            con.requestMethod = "GET"
+            con.connect()
+            val stream = con.inputStream
+            val result = is2String(stream)
+            con.disconnect()
+            stream.close()
+            return result
+        }
+
+        override fun onPostExecute(result: String) {
+            val rootJSON = JSONObject(result)
+            _from.add(rootJSON.getString("elevation"))
+            val EndElevationReceiver = EndElevationReceiver()
+            EndElevationReceiver.execute(_endLng, _endLat)
+        }
+    }
+
+    // 区間ごとの到着地となるバス停の標高を求める
+    private inner class EndElevationReceiver() : AsyncTask<String, String, String>() {
+        var _endLng = ""
+        var _endLat = ""
+        override fun doInBackground(vararg params: String): String {
+            _endLng = params[0]
+            _endLat = params[1]
+            val urlStr =
+                "http://cyberjapandata2.gsi.go.jp/general/dem/scripts/getelevation.php?lon=${_endLng}&lat=${_endLat}&outtype=JSON"
+            val url = URL(urlStr)
+            val con = url.openConnection() as HttpURLConnection
+            con.requestMethod = "GET"
+            con.connect()
+            val stream = con.inputStream
+            val result = is2String(stream)
+            con.disconnect()
+            stream.close()
+            return result
+        }
+
+        override fun onPostExecute(result: String) {
+            val rootJSON = JSONObject(result)
+            _to.add(rootJSON.getString("elevation"))
+            val EndElevationReceiver = EndElevationReceiver()
+            EndElevationReceiver.execute(_endLng, _endLat)
+            val counter = _counter
+            val heightDif = _to[_counter].toFloat() - _from[_counter].toFloat()
+            val dpt = _to
+            val dst = _from
+            _heightDifs.add(heightDif)
+            val value = getValue(_walkingTimes[_counter].toInt(), _timeDifs[_counter])
+            val secName = _busStopNames[_counter] + "~" + _busStopNames[_counter+1]
+            _valueList.add(mutableMapOf("secName" to secName, "value" to value))
+            val valueList = _valueList
+            _counter = _counter + 1
+            val from = arrayOf("secName", "value")
+            val to = intArrayOf(android.R.id.text1, android.R.id.text2)
+            val adapter = SimpleAdapter(applicationContext, _valueList, android.R.layout.simple_list_item_2, from, to)
+            val lvValue = findViewById<ListView>(R.id.lvValue)
+            lvValue.adapter = adapter
+        }
+    }
+
+    private fun is2String(stream: InputStream): String {
+        val sb = StringBuilder()
+        val reader = BufferedReader(InputStreamReader(stream, "UTF-8"))
+        var line = reader.readLine()
+        while (line != null) {
+            sb.append(line)
+            line = reader.readLine()
+        }
+        reader.close()
+        return sb.toString()
+    }
+    // 各区間のバス移動時間を秒単位で返却する関数
+    private fun getTimeDif(time1: String, time2: String): Int {
+        val sdf = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+        val date1 = sdf.parse("2019/01/01 ${time1}:00")
+        val date2 = sdf.parse("2019/01/01 ${time2}:00")
+        val dateTimeTo = date2.time
+        val dateTimeFrom = date1.time
+        val Longdif = (dateTimeTo - dateTimeFrom) / 1000
+        val dif = Longdif.toInt()
+        return dif
+    }
+
+    private fun getValue(facter1: Int, facter2: Int): Int {
+        return facter1 + facter2
+    }
+}
