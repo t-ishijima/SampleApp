1e28421811faabc2497e90219770e08eb7ef6913
ishijima <cgebtaro0221@gmail.com>
2019-11-08 17:38:46 +0900

    add ResultDetailActivity

---
+++ app/src/main/java/com/websarva/wings/android/appsample/ResultDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.websarva.wings.android.appsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_detail)
+    }
+}
