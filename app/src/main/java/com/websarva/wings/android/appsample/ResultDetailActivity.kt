1fd7fad3fd324ac6ea86a275165f99adeaf52bc1
ishijima <cgebtaro0221@gmail.com>
2019-11-09 19:56:17 +0900

    データ詳細画面に必要な全データを渡して画面に表示

--- app/src/main/java/com/websarva/wings/android/appsample/ResultDetailActivity.kt
+++ app/src/main/java/com/websarva/wings/android/appsample/ResultDetailActivity.kt
@@ -2,6 +2,8 @@ package com.websarva.wings.android.appsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_result_detail.*
 
 class ResultDetailActivity : AppCompatActivity() {
 
@@ -10,11 +12,33 @@ class ResultDetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_result_detail)
 
         val secName = intent.getStringExtra("secName")
-        val value = intent.getStringExtra("value")
-        val temp = intent.getStringExtra("temp").toDouble()
-        val heightDif = intent.getStringExtra("heightDif")
-        val distanceText = intent.getStringExtra("distanceText")
-        val walkingTime = intent.getStringExtra("walkingTime")
-        val timeDif = intent.getStringExtra("timeDif")
+        val value = intent.getStringExtra("value") + "ポイント"
+        val temp = Math.floor(intent.getStringExtra("temp").toDouble()).toString() + "度"
+        val heightDif = formatHeightDif(intent.getStringExtra("heightDif"))
+        val distance = intent.getStringExtra("distanceText") + "m"
+        val walkingTime = formatTime(intent.getStringExtra("walkingTime"))
+        val timeDif = formatTime(intent.getStringExtra("timeDif"))
+        val tvSecName = findViewById<TextView>(R.id.tvSecName)
+        val tvValue = findViewById<TextView>(R.id.tvValue)
+        val tvTemp = findViewById<TextView>(R.id.tvTemp)
+        val tvHeightDif = findViewById<TextView>(R.id.tvHeightDif)
+        val tvDistance = findViewById<TextView>(R.id.tvDistance)
+        val tvWalkingTime = findViewById<TextView>(R.id.tvWalkingTime)
+        val tvBustimeDif = findViewById<TextView>(R.id.tvBustimedif)
+        tvSecName.setText(secName)
+        tvValue.setText(value)
+        tvTemp.setText(temp)
+        tvHeightDif.setText(heightDif)
+        tvDistance.setText(distance)
+        tvWalkingTime.setText(walkingTime)
+        tvBustimeDif.setText(timeDif)
+    }
+
+    private fun formatHeightDif(value: String) : String {
+        return ((Math.floor(value.toDouble() * 100)) / 100).toString() + "m"
+    }
+
+    private fun formatTime(value: String) : String {
+        return (Math.round(value.toFloat()) / 60).toString() + "分"
     }
 }
